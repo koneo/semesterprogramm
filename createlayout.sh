620fca6e21fb0c8ada18f9aa012ff67e599b76d5
Koneo WB-Si05! WB-Kö11! <koneo@nibelungen-siegen.de>
2016-10-27 06:14:15 +0200

    changed layout for wir-machen-druck

--- createlayout.sh
+++ createlayout.sh
@@ -9,16 +9,16 @@ xelatex -synctex=1 -interaction=nonstopmode "main".tex
 pdftk main.pdf burst
 
 # Layouting for Wir-machen-druck.de 
-pdftk pg_0005.pdf pg_0006.pdf pg_0007.pdf pg_0008.pdf cat output tmp_A4_aussen.pdf
-pdfjam --quiet --landscape tmp_A4_aussen.pdf --papersize '{111mm,295mm}' --nup 4x1  --outfile final_wirmachendruck_aussen.pdf
-pdftk pg_0002.pdf pg_0001.pdf pg_0003.pdf pg_0004.pdf cat output tmp_A4_innen.pdf
+pdftk pg_0004.pdf pg_0003.pdf pg_0002.pdf pg_0001.pdf cat output tmp_A4_seitemittitel.pdf
+pdfjam --quiet --landscape tmp_A4_seitemittitel.pdf --papersize '{111mm,295mm}' --nup 4x1  --outfile final_wirmachendruck_seitemittitel.pdf
+pdftk pg_0005.pdf pg_0006.pdf pg_0007.pdf pg_0008.pdf cat output tmp_A4_innen.pdf
 pdfjam --quiet --landscape tmp_A4_innen.pdf --papersize '{111mm,295mm}' --nup 4x1  --outfile final_wirmachendruck_innen.pdf
 
 # Layouting for wingolfsplattform.org 
 pdftk pg_0001.pdf pg_0002.pdf pg_0003.pdf pg_0004.pdf pg_0005.pdf pg_0006.pdf pg_0007.pdf pg_0008.pdf cat output final_online_wingolfsplattform.pdf
 
 # Layouting for "Do-It-Yourself"
-pdfjam --quiet --landscape tmp_A4_innen.pdf tmp_A4_aussen.pdf --paper A4paper --nup 4x2  --outfile final_online_diy.pdf
+pdfjam --quiet --landscape tmp_A4_innen.pdf tmp_A4_seitemittitel.pdf --paper A4paper --nup 4x2  --outfile final_online_diy.pdf
 
 trash main.pdf
 trash main.aux
